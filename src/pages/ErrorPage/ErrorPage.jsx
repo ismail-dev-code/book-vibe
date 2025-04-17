71711c65d3d1e285af395a3c864eeb6a0aa5f983
ismail-dev-code <ismail.online0385678@gmail.com>
2025-04-17 08:17:28 +0600

    navbar added

---
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ErrorPage = () => {
+    return (
+        <div>
+            <p className='text-red-700'><small>page not found re ba</small></p>
+        </div>
+    );
+};
+
+export default ErrorPage;
